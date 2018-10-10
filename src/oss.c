7cdd92a1c698dd137a2dd9dfcfa19891bd62b0bc
Bilal Ather <bilal.ather@ocelotconsulting.com>
2018-10-09 21:10:11 -0500

    added shared memory setup

--- src/oss.c
+++ src/oss.c
@@ -9,8 +9,19 @@
 #include <signal.h>
 #include "validation.h"
 
+
+// log files
+char* log_filename;
+FILE* log_fp = NULL;
+
+// shared memory
+int shmClockId;
+int shmMsgId;
+void* shmClockAddress;
+void* shmMsgAddress;
+
 int main(int argc, char *argv[]){
-  int maxRunTime;
+  int maxRunTime = 2;
 
   static char usageMessage[] = "%s: Usage: %s [-s maximum number of user processes spawned (Default 5)] [-l log file name] [-t max run time][-h help]\n";
   static char helpMessage[] = "-s  Maximum number of user processes spawned Default 5.\n\
@@ -21,28 +32,62 @@ int main(int argc, char *argv[]){
 // alarm(2);
 // signal(SIGALRM, timeoutKillAll);
 // signal(SIGINT, handleCtrlC);
-int customErrorCode, n, option;
+int customErrorCode, option;
+int maxProcesses = -1;
 extern char *optarg;
 //TODO: Add colons
+
 while ((option = getopt(argc, argv, "hslt")) != -1) {
   switch (option) {
     case('h'):
      printf("%s%s\n", argv[0], helpMessage);
      return 0;
-   case('n'):
+   case('s'):
      if (isNumber(optarg)) {
-       n = (int)strtol(optarg, NULL, 0);
+       maxProcesses = (int)strtol(optarg, NULL, 0);
      } else {
        customErrorCode = 1;
      }
      break;
-   case('s'):
-     printf("option s needs to be implemented\n");
+   case('l'):
+     log_fp = fopen(optarg, "w");
+     log_filename = optarg;
+     break;
+   case('t'):
+     if (isNumber(optarg)) {
+       maxRunTime = (int)strtol(optarg, NULL, 0);
+     } else {
+       customErrorCode = 2;
+     }
      break;
    case ('?'):
      customErrorCode = 3;
      break;
   }
 }
+// set to default
+if (maxProcesses == -1) {
+  maxProcesses = 5;
+}
+// alarm(maxRunTime);
+key_t shmClockKey = ftok("./CreateKeyFile", 1);
+key_t shmMsgKey = ftok("./CreateKeyFile", 2);
+key_t semKey = ftok("./CreateKeyFile", 3);
+
+shmClockId = shmget(shmClockKey, sizeof(int) * 2, IPC_CREAT | 0644);
+shmMsgId= shmget(shmMsgKey, sizeof(int) * 3, IPC_CREAT | 0644);
+
+
+shmMsgAddress = shmat(shmClockId, (void*)0, 0);
+shmClockAddress = shmat(shmMsgId, (void*)0, 0);
+
+// setup clock
+int* shmClock = shmClockAddress;
+int* shmMsg   = shmMsgAddress;
+
+shmClock[0] = 0, shmClock[1] = 0; //subscript 0 -> seconds, subscript 1 -> nanoseconds
+shmMsg[0] = -1, shmMsg[1] = -1, shmMsg[2] = -2; //subscript 0 -> seconds, subscript 1 -> nanoseconds, subscript 2 -> PID
+
+
 
 }
